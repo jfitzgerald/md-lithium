9ee151e12bccff7b942bf9e81d4686dc4c429565
Do Pham <dopham@Dos-MacBook-Pro.local>
2017-05-02 11:02:36 -0700

    retry anchor fix

--- md-lithium.js
+++ md-lithium.js
@@ -96,7 +96,7 @@ var editLithium = function(line, outputTarget, outputDraft) {
   }
   // Forcing all links to open new tab
   // EXCEPT anchor jumps
-  if ((line.indexOf('a href')) !== -1 && (line.indexOf('(#') === -1)) {
+  if ((line.indexOf('a href')) !== -1 && (line.indexOf('href="#') === -1)) {
     line = line.replace('a href', 'a target="_blank" href');
   }
   outputTarget.write(line);
